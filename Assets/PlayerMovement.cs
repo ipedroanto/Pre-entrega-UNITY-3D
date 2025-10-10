a410f227ac621f84400b462afa8c3dc32ad70e6c
ipedroanto <76076226+ipedroanto@users.noreply.github.com>
2025-10-10 12:25:15 -0300

    Character controller use 10/10/2025

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+
+    private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -19,9 +21,11 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude =  Mathf.Clamp01(movementDirection.magnitude) * speed;
+        
         movementDirection.Normalize();
 
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        characterController.SimpleMove(movementDirection * magnitude);
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
